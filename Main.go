0717fcecd40f19a115123f02b27a94c6525c0664
Wesley Lewis <lewis.wes.23@gmail.com>
2022-09-24 15:48:39 +0530

    Complted the file compression code

--- Main.go
+++ Main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"bufio"
 	"compress/gzip"
-	"fmt"
 	"io/ioutil"
+	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
 	name_of_file := "file1.txt"
+
 	f, _ := os.Open("C:\\Users\\wesle\\OneDrive\\Desktop\\go\\src\\file_compression\\" + name_of_file)
 
 	read := bufio.NewReader(f)
@@ -20,4 +21,12 @@ func main() {
 	name_of_file = strings.Replace(name_of_file, ".txt", ".gz", -1)
 
 	f, _ = os.Create("C:\\Users\\wesle\\OneDrive\\Desktop\\go\\src\\file_compression\\" + name_of_file)
+
+	w := gzip.NewWriter(f)
+
+	w.Write(data)
+
+	w.Close()
+
+	fmt.Println("File is converted succesfully")
 }
